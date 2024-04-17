ba9c54701733d8f9ebf975e16bbdfe1c3115490d
bdaiab <binyang.dby@gmail.com>
2024-04-17 23:06:25 +0800

    Fix the empty result issue

--- sqlplus-core/src/main/scala/sqlplus/gyo/GyoAlgorithm.scala
+++ sqlplus-core/src/main/scala/sqlplus/gyo/GyoAlgorithm.scala
@@ -235,7 +235,11 @@ class GyoAlgorithm {
             })
 
             val finalCandidates = if (outputVariables.nonEmpty) candidates.filter(c => c._1.root.getNodes().intersect(outputVariables).nonEmpty) else candidates
-            val filteredCandidates = if (pruneEnable) finalCandidates.filter(t => t._1.getEdges().count(e => e.getSrc == t._1.root || e.getDst == t._1.root) < 4) else finalCandidates
+            val filteredCandidates = if (pruneEnable)
+                finalCandidates.filter(t => !t._1.isFixRoot || (t._1.getEdges().count(e => e.getSrc == t._1.root || e.getDst == t._1.root) < 4))
+            else
+                finalCandidates
+
             GyoResult(filteredCandidates, isFreeConnex, List.empty)
         } else {
             throw new IllegalStateException("some final states have more than 1 relation")
